8f702d78745b0539916f0f59bac692b1a467df68
Daniel Bernau <daniel.bernau@sap.com>
2021-07-28 23:42:22 +0200

    Refactored Adult dataset code and notebooks

--- dpa/projects/adult/run_experiment.sh
+++ dpa/projects/adult/run_experiment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 runs=5
 for ((i=1;i<=runs;i++)); do
-    python ./run_experiment.py -it=1 -ep=5 -l2=3.0 -ls -dlt=0.01 -lr=0.01 -bnd -id=$i
+    python ./run_experiment.py -it=1 -ep=30 -l2=3.0 -ls -dlt=0.001 -lr=0.005 -bnd -id=$i
 done
 sudo /sbin/shutdown -h now
